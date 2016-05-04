87e946173604157a8de52ba25d0635049db47892
Maksim Luzik <maksim.luzik@captalize.com>
2016-05-04 14:40:25 +0300

    json response

--- routes/items.js
+++ routes/items.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ item: "blaa", thisIsObject: 1, something: "jeah!!122" }));
+    res.send(JSON.stringify({ item: { text: "this is the TEXT!", something: "jeah!!122" } }));
 });
 
 module.exports = router;
