66b9a1b44dbed4921dfac82bce73849a3408edab
Maksim Luzik <maksim.luzik@captalize.com>
2016-05-04 12:01:07 +0300

    express changes

--- routes/items.js
+++ routes/items.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('jeah!');
+  res.send('jeah! this is cool!123');
 });
 
 module.exports = router;
