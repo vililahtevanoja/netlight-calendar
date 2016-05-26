873c454893f69453f372146967a7c20c5f386356
Maksim Luzik <maksim.luzik@gmail.com>
2016-05-26 13:57:10 +0300

    abstractized calendars

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,71 +3,152 @@
  */
 $(document).ready(function() {
 
-  var aspectRatio = 0.95;
+  var aspectRatio = 5.0;
   var minTime = "06:00:00";
   var maxTime = "20:00:00";
-  var defaultView = 'agendaDay';
+  var defaultView = 'timelineDay';
   var timeFormat = 'HH:mm';
   var color = '#281B48';
   var textColor = 'white';
+  var refreshInterval = 60 * 5; // 5minutes
 
   // page is now ready, initialize the calendar...
 
-  $('#calendar1').fullCalendar({
-    // put your options and callbacks here
-    header: {
-      left: '',
-      center: '',
-      right: '',
-    },
-    handleWindowResize: false,
-    defaultView: defaultView,
-    aspectRatio: aspectRatio,
-    weekends: false,
-    axisFormat: timeFormat,
-    minTime: minTime,
-    maxTime: maxTime,
-    timezone: 'UTC',
+  // Variable calendars is global and is loaded from the calendars.js file
+  $.getJSON("/calendars.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
 
-    eventSources: [
+    var calendars = json;
+    var eventResources = [];
+    var roomResources = [];
+    for (var calendar in calendars){
+      if (calendars.hasOwnProperty(calendar)) {
+        var current = calendars[calendar];
+        eventResources.push(
+          {
+            url: '/events/' + current.resourceId,
+            color: color,
+            textColor: textColor,
+          }
+        );
+        roomResources.push(
+          {
+            id: current.resourceId,
+            title: current.title
+          }
+        )
+      }
+    }
 
-      // your event source
-      {
-        url: '/events/pop_corner', // use the `url` property
-        color: color,    // an option!
-        textColor: textColor,  // an option!
+    $('#calendar').fullCalendar({
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+      // put your options and callbacks here
+      header: {
+        left: '',
+        center: '',
+        right: '',
       },
-    ]
+      handleWindowResize: false,
+      defaultView: defaultView,
+      aspectRatio: aspectRatio,
+      weekends: false,
+      axisFormat: timeFormat,
+      minTime: minTime,
+      maxTime: maxTime,
+      timezone: 'UTC+3',
+
+      eventSources: eventResources,
+      resourceLabelText: 'Rooms',
+      resources: roomResources
+
+    });
+
+    setInterval(function(){$('#calendar').fullCalendar('refetchEvents')}, refreshInterval * 1000);
+
+    //$('#calendar2').fullCalendar({
+    //  schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+    //  // put your options and callbacks here
+    //  header: {
+    //    left: '',
+    //    center: '',
+    //    right: '',
+    //
+    //  },
+    //  handleWindowResize: false,
+    //  defaultView: defaultView,
+    //  aspectRatio: aspectRatio,
+    //  weekends: false,
+    //  axisFormat: timeFormat,
+    //  minTime: minTime,
+    //  maxTime: maxTime,
+    //  timezone: 'UTC',
+    //
+    //  eventSources: [
+    //
+    //    {
+    //      url: '/events/internet_cafe', // use the `url` property
+    //      color: color,    // an option!
+    //      textColor: textColor,  // an option!
+    //    },
+    //  ]
+    //});
+
+    setTimelineRec();
+    setInterval(function () {
+      $('.timeline').remove();
+      setTimelineRec();
+    }, 5000);
+
+    $(window).on('resize', function () {
+      $('.timeline').remove();
+      setTimelineRec();
+    });
 
   });
 
-  $('#calendar2').fullCalendar({
-    // put your options and callbacks here
-    header: {
-      left: '',
-      center: '',
-      right: '',
+  function setTimelineRec(view) {
+    // Get the div where the line will be shown.
+    var parentDivAux = jQuery(".fc-time-area.fc-widget-content").children().children().children().children();
+    var parentDiv = jQuery();
+    for (var i = 0; i < parentDivAux.length; i++) {
+      // Need only (i think so) the fc-bg, cause fc-content is defined on multiple occasions and line would be painted multiple times.
+      if (parentDivAux[i].className === "fc-bg") {
+        parentDiv.push(parentDivAux[i]);
+      }
+    }
 
-    },
-    handleWindowResize: false,
-    defaultView: defaultView,
-    aspectRatio: aspectRatio,
-    weekends: false,
-    axisFormat: timeFormat,
-    minTime: minTime,
-    maxTime: maxTime,
-    timezone: 'UTC',
+    var timeline = parentDiv.children(".timeline").children();
+    if (timeline.length == 0) { //if timeline isn't there, add it
+      timeline = jQuery("<hr>").addClass("timeline");
 
-    eventSources: [
+      // My calendar starts at 9 am and finishes at 9 pm, so I get the total amount of seconds from midnight to 9 am and from 9 am to 9 pm
+      var secondsHourMin = 9 * 3600;
 
-      {
-        url: '/events/internet_cafe', // use the `url` property
-        color: color,    // an option!
-        textColor: textColor,  // an option!
-      },
-    ]
-  });
+      var totalSeconds = 12 * 3600 ;
 
+      // Get the width of the div where the line will be painted
+      var widthAux = jQuery(".fc-time-area.fc-widget-content").children().children().children();
+      var width = widthAux.width();
+
+      // We obtain the current time/date to know where we should draw the vertical line
+      var curTime = new Date();
+      var curSeconds = (curTime.getHours() * 60 * 60) + (curTime.getMinutes() * 60) + curTime.getSeconds() - secondsHourMin;
+      var percentOfDay = curSeconds / totalSeconds; //totalSeconds = 12 hours
+
+      // Calculate the margin with a known width and the actual percentOfDay
+      var margin = (percentOfDay * width);
+
+      // Margin is applied and finally I prepend the timeline.
+      timeline.css({
+        left: margin + "px",
+        height: "100%",
+        width: 1 + "px"
+      });
+
+      parentDiv.prepend(timeline);
+    }
+
+  }
 
 
 });
--- routes/events.js
+++ routes/events.js
@@ -1,20 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var ical = require('ical');
+var fs = require("fs");
 const isset = require('isset');
+
+var contents = fs.readFileSync('./config/calendars.json');
+var calendars = JSON.parse(contents);
 
 var TIMEZONE = 'VTIMEZONE';
 var EVENT = 'VEVENT';
-
-// Add here the url of the calendars that you want to support
-var calendars = {
- pop_corner: {
-   url: 'http://outlook.office365.com/owa/calendar/4048201c9bac4cb0abe6d5c5865d8fc1@netlight.fi/7d24a835be974460901686ec348a52c613748629002781753650/calendar.ics',
- },
-  internet_cafe: {
-    url: 'http://outlook.office365.com/owa/calendar/17722c88f8fa46c4b213104744ca2d6f@netlight.fi/db804571ccbc42eb8c2077a294ad0b12225485023870966913/calendar.ics',
-  }
-}
 
 /* GET users listing. */
 router.get('/:calendar', function(req, res, next) {
@@ -31,6 +25,7 @@ router.get('/:calendar', function(req, res, next) {
             console.log("START", ev.start);
             console.log("END", ev.end);
             ev.title = ev.summary;
+            ev.resourceId = calendars[req.params.calendar].resourceId;
             response.push(ev);
           }
         }
